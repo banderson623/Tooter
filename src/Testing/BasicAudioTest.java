28bb7e16558aaa0499eb201d696f03a2b2442b81
Brian Anderson <banderson623@gmail.com>
2013-04-13 23:04:57 -0500

    basic support for a wav file

---
+++ src/Testing/BasicAudioTest.java
@@ -0,0 +1,17 @@
+package Testing;
+
+/**
+ * User: brian_anderson
+ * Date: 4/13/13
+ * <p/>
+ * Add some readme here about how this operates
+ */
+public class BasicAudioTest {
+
+    public static void main(String[] args){
+        System.out.println("Hello!");
+        //Instrument piano = new Piano();
+        //piano.play(piano.getSupportedNotes().get(0));
+
+    }
+}
--- src/instruments/InstrumentBase.java
+++ src/instruments/InstrumentBase.java
@@ -1,5 +1,11 @@
 package instruments;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+import java.net.URL;
+
 /**
  * User: brian_anderson
  * Date: 4/13/13
@@ -9,13 +15,23 @@ package instruments;
 public abstract class InstrumentBase implements Instrument{
     protected void playSoundFileFromPath(String filePath){
         // TODO: do the work of playing the sound file...
-//        Player player = Manager.createPlayer(new MediaLocator(getClass().getClassLoader().getResource("Sounds/" + filePath)));
-
+        try {
+            String fullPath = getClass().getClassLoader().getResource(".").getPath();
+            fullPath += "/piano/c.wav";
+            File soundFile = new File(fullPath);
+            System.out.println("defaultSound " + filePath);  // check the URL!
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new URL(fullPath));
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start( );
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
 
     @Override
     public void play(Note noteToPlay){
-        playSoundFileFromPath(noteToPlay.getFileForNote());
+        playSoundFileFromPath(noteToPlay.getFileName());
     }
 }
