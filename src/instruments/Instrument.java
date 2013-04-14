132cf7634ed0d9a1f9dcaa71611e716334906175
Tae Berger <taeberger@gmail.com>
2013-04-13 22:03:11 -0500

    test

--- src/instruments/Instrument.java
+++ src/instruments/Instrument.java
@@ -11,14 +11,26 @@ import java.util.List;
 public interface Instrument {
 
     public interface Note {
-        public String name();
+        public String getName();
+        public String getFileForNote();
     }
 
-
+    /**
+     * Plays the note.
+     * @param noteToPlay
+     */
     public void play(Note noteToPlay);
 
+    /**
+     * The name of the instrument
+     * @return the String name of the instrument
+     */
+    public String getName();
 
-    // At some point might need to play a note
+    /**
+     * List of notes that this instruments can play
+     * @return a list of Note Interface types
+     */
     public List<Note> getSupportedNotes();
 
 }
