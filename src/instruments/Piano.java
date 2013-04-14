2b76e85557542f9db3c096802ce445487f820638
Brian Anderson <banderson623@gmail.com>
2013-04-13 21:52:55 -0500

    simple instrument classes

---
+++ src/instruments/Piano.java
@@ -0,0 +1,53 @@
+package instruments;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * User: brian_anderson
+ * Date: 4/13/13
+ * <p/>
+ * Add some readme here about how this operates
+ */
+public class Piano extends InstrumentBase {
+
+    public class NoteA implements Note {
+
+        @Override
+        public String getName()
+        {
+            return "A";
+        }
+    }
+
+    public class NoteB implements Note {
+
+        @Override
+        public String getName()
+        {
+            return "B";
+        }
+    }
+
+
+    @Override
+    public void play(Note noteToPlay)
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public String getName()
+    {
+        return "Baby Grand Piano";
+    }
+
+    @Override
+    public List<Note> getSupportedNotes()
+    {
+        List<Note> myNotes = new LinkedList<Note>();
+        myNotes.add(new NoteA());
+        myNotes.add(new NoteB());
+        return myNotes;
+    }
+}
