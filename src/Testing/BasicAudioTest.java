8e4298ae66c490d13795d6e39836218f002e7aae
Brian Anderson <banderson623@gmail.com>
2013-04-13 23:39:25 -0500

    added delay playing

--- src/Testing/BasicAudioTest.java
+++ src/Testing/BasicAudioTest.java
@@ -3,6 +3,8 @@ package Testing;
 import instruments.Instrument;
 import instruments.Piano;
 
+import java.util.Random;
+
 /**
  * User: brian_anderson
  * Date: 4/13/13
@@ -11,12 +13,26 @@ import instruments.Piano;
  */
 public class BasicAudioTest {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException
+    {
         System.out.println("Hello!");
         Instrument piano = new Piano();
         piano.play(piano.getSupportedNotes().get(0));
+        Thread.sleep(1000);
         piano.play(piano.getSupportedNotes().get(1));
-        piano.play(piano.getSupportedNotes().get(2));
-        piano.play(piano.getSupportedNotes().get(3));
+
+        Random r = new Random();
+
+        for(int i = 0; i < 10; i++){
+            Thread.sleep(200 * r.nextInt(5));
+            piano.play(piano.getSupportedNotes().get(r.nextInt(4)));
+
+        }
+        Thread.sleep(3000);
+
+
+//        piano.play(piano.getSupportedNotes().get(1));
+//        piano.play(piano.getSupportedNotes().get(2));
+//        piano.play(piano.getSupportedNotes().get(3));
     }
 }
