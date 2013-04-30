569d12f3508db3d7d2740d9b3f71831321007cfd
Brian Anderson <banderson623@gmail.com>
2013-04-29 22:29:15 -0500

    test code for sniffy

--- src/Testing/TestDiscovery.java
+++ src/Testing/TestDiscovery.java
@@ -1,5 +1,10 @@
 package Testing;
 
+import com.digitalxyncing.communication.Sniffy;
+import com.digitalxyncing.communication.impl.ZmqSniffy;
+
+import java.util.List;
+
 /**
  * User: brian_anderson
  * Date: 4/29/13
@@ -7,4 +12,17 @@ package Testing;
  * Add some readme here about how this operates
  */
 public class TestDiscovery {
+
+    public static void main(String[] args) {
+
+
+        Sniffy sniff = new ZmqSniffy();
+        List<String> hosts =  sniff.discoverXyncersOnPort(5050);
+
+
+        System.out.println("Found: " + hosts.size() + " hosts");
+        for(String h : hosts){
+            System.out.println(h);
+        }
+    }
 }
