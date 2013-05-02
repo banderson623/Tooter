3b5091618b171ffed2aa179bbd2d279f2f430ab1
Tyler Treat <ttreat31@gmail.com>
2013-05-02 13:40:30 -0500

    Remove generic typing from Endpoint and ChannelListener.

--- src/messaging/SongMessageHandlerFactory.java
+++ src/messaging/SongMessageHandlerFactory.java
@@ -4,10 +4,10 @@ import com.digitalxyncing.communication.Endpoint;
 import com.digitalxyncing.communication.MessageHandler;
 import com.digitalxyncing.communication.MessageHandlerFactory;
 
-public class SongMessageHandlerFactory implements MessageHandlerFactory<Song> {
+public class SongMessageHandlerFactory implements MessageHandlerFactory {
 
     @Override
-    public MessageHandler build(Endpoint<Song> endpoint, byte[] bytes) {
+    public MessageHandler build(Endpoint endpoint, byte[] bytes) {
         return new SongMessageHandler(endpoint, bytes);
     }
 
